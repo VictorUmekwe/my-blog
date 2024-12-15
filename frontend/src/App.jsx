504d33148ae94f5c3743559be581b82bfcd7141e
Umekwe Victor <drealchigozie2000@yahoo.com>
2024-12-15 17:06:21 +0100

    Created my react app with vite  and installed react bootstrap

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+         <Route path='/' element={<Home/>}/>
+         <Route path='/about' element={<About/>}/> 
+         <Route path='/sign-up' element={<SignUp />} />
+         <Route path='/sign-in' element={<SignIn/>} />
+         <Route path='/dashboard' element={<Dashboard/>}/>
+         <Route path='/projects' element={<Projects/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
