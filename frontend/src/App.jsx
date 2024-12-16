de265755231ce16ad7968916e8b0123b97144896
Umekwe Victor <drealchigozie2000@yahoo.com>
2024-12-16 13:07:40 +0100

    implemented ligh/dark mode

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,52 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import SignUp from './pages/SignUp'
-import SignIn from './pages/SignIn'
-import Dashboard from './pages/Dashboard'
-import Projects from './pages/Projects'
-import Header from './components/Header'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import Header from "./components/Header";
+import { useState } from "react";
 
 const App = () => {
-   
+  const [mode, setMode] = useState("light");
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#494F55";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+    }
+  };
   return (
     <BrowserRouter>
-    <Header />
+      <Header mode={mode} toggleMode={toggleMode} />
       <Routes>
-         <Route path='/' element={<Home/>}/>
-         <Route path='/about' element={<About/>}/> 
-         <Route path='/sign-up' element={<SignUp />} />
-         <Route path='/sign-in' element={<SignIn/>} />
-         <Route path='/dashboard' element={<Dashboard/>}/>
-         <Route path='/projects' element={<Projects/>}/>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/about"
+          element={<About mode={mode} toggleMode={toggleMode} />}
+        />
+        <Route
+          path="/sign-up"
+          element={<SignUp mode={mode} toggleMode={toggleMode} />}
+        />
+        <Route
+          path="/sign-in"
+          element={<SignIn mode={mode} toggleMode={toggleMode} />}
+        />
+        <Route
+          path="/dashboard"
+          element={<Dashboard mode={mode} toggleMode={toggleMode} />}
+        />
+        <Route
+          path="/projects"
+          element={<Projects mode={mode} toggleMode={toggleMode} />}
+        />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,12 +5,12 @@ import { Link, useLocation } from "react-router-dom";
 import { Button, Form, InputGroup } from "react-bootstrap";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa";
-import '../css/Header.css'
+import "../css/Header.css";
 
-const Header = () => {
-  const path = useLocation().pathname
+const Header = ({ mode, toggleMode }) => {
+  const path = useLocation().pathname;
   return (
-    <Navbar expand="lg" className=" border-bottom" >
+    <Navbar expand="lg" className={`border-bottom ${mode}`}>
       <Container>
         <Navbar.Brand href="#home" className="fs-5 fw-bold">
           <span className=" px-2 py-1 bg-dark bg-gradient rounded text-light">
@@ -34,17 +34,48 @@ const Header = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" className=" order-2" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mx-auto text-center py-2 ">
-            <Nav.Link as={Link} to={'/'}className="nav-links" active={path === '/'}>Home</Nav.Link>
-            <Nav.Link as={Link} to={'/about'} className="nav-links" active={path === '/about'}>About</Nav.Link>
-            <Nav.Link as={Link} to={'/projects'} className="nav-links" active={path === '/projects'}>Projects</Nav.Link>
+            <Nav.Link
+              as={Link}
+              to={"/"}
+              className="nav-links"
+              active={path === "/"}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to={"/about"}
+              className="nav-links"
+              active={path === "/about"}
+            >
+              About
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to={"/projects"}
+              className="nav-links"
+              active={path === "/projects"}
+            >
+              Projects
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
-        <button className="btn btn-outline-dark rounded-pill me-3" >
-          <FaMoon height={10} width={12} />
-        </button>
-        <Link to='/sign-in'>
-          <button className="btn btn-outline-dark">Sign In</button>
+        <Link to="/sign-in">
+          <button className="btn btn-outline-dark me-3">Sign In</button>
         </Link>
+        <div className="form-check form-switch">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            role="switch"
+            id="light/dark"
+            onClick={toggleMode}
+          />
+          {/* <label className="form-check-label" htmlFor="light/dark">
+           
+            light/dark
+          </label> */}
+        </div>
       </Container>
     </Navbar>
   );
