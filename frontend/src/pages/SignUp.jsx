929ae2d654844c9ded3caceeac1f60c80c1b7df7
Umekwe Victor <drealchigozie2000@yahoo.com>
2024-12-17 20:08:49 +0100

    added functionality to the sign up page

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,6 +1,58 @@
-import {Link} from 'react-router-dom'
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleUsername = (e) => {
+    setFormData({ ...formData, username: e.target.value.trim() });
+  };
+
+  const handleEmail = (e) => {
+    setFormData({ ...formData, email: e.target.value.trim() });
+  };
+
+  const handlePassword = (e) => {
+    setFormData({ ...formData, password: e.target.value.trim() });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // connecting to backend route for signup
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage("Please fill out all fields");
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      //error message
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      // navigate to sign in page if sign up is successful
+      if(res.ok){
+        navigate('/sign-in');
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
   return (
     <div
       className=" container shadow-lg border-2"
@@ -16,7 +68,6 @@ const SignUp = () => {
               Blog
             </p>
             <p className=" fs-3">
-              {" "}
               Welcome to My blog where get the latest news in tech
             </p>
           </div>
@@ -24,12 +75,14 @@ const SignUp = () => {
 
         <div className=" col-md-6 mt-sm-4">
           <div>
-            <form>
+            <form onSubmit={handleSubmit}>
               <div className="form-floating mb-3">
                 <input
                   type="text"
                   className="form-control"
-                  placeholder="Enter your email"
+                  placeholder="Enter your username"
+                  value={formData.username}
+                  onChange={handleUsername}
                 />
                 <label>Username</label>
               </div>
@@ -39,6 +92,8 @@ const SignUp = () => {
                   type="email"
                   className="form-control"
                   placeholder="Enter your email"
+                  value={formData.email}
+                  onChange={handleEmail}
                 />
                 <label>Email</label>
               </div>
@@ -48,23 +103,33 @@ const SignUp = () => {
                   type="password"
                   className="form-control"
                   placeholder="Enter your password"
+                  value={formData.password}
+                  onChange={handlePassword}
                 />
                 <label>Password</label>
               </div>
 
               <button
                 type="submit"
-                className="btn  w-100 text-center text-white btn-dark mt-2 rounded-pill fw-bold"
+                className="btn  w-100 text-center text-white btn-dark mt-2 rounded-pill fw-bold" disabled={loading}
               >
-                Sign In
+                {
+                  loading ? (<div className="spinner-border" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                  </div>) : 'Sign Up'
+                }
               </button>
             </form>
-            <div className=' mt-2 d-flex gap-2 fs-6'>
+            <div className=" mt-2 d-flex gap-2 fs-6">
               <span>Have an account?</span>
-               <Link to='sign-in'>
-               Sign In
-               </Link>
+              <Link to="sign-in">Sign In</Link>
             </div>
+
+            {errorMessage && (
+              <div className="alert alert-danger" role="alert">
+                {errorMessage}
+              </div>
+            )}
           </div>
         </div>
       </div>
